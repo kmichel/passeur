3bfac3eff7a193bc2598420c1ff32c13545e3c53
Kevin Michel <kha.hem@gmail.com>
2014-08-23 07:38:23 +0200

    Navigate between generated islands with the keyboard

---
+++ Assets/IslandsNavigator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IslandsNavigator : MonoBehaviour {
+
+	public IslandGenerator generator;
+
+	[System.NonSerialized]
+	private string seed;
+
+	void Update () {
+		if (seed == null)
+			seed = "";
+
+		if (Input.GetKeyDown("left"))
+			UpdateSeed("l", "r");
+		if (Input.GetKeyDown("right"))
+			UpdateSeed("r", "l");
+		if (Input.GetKeyDown("up"))
+			UpdateSeed("t", "b");
+		if (Input.GetKeyDown("down"))
+			UpdateSeed("b", "t");
+
+		if (generator != null)
+			generator.seed = seed;
+	}
+
+	public void UpdateSeed(string suffix, string oppositeSuffix) {
+		if (seed == null)
+			seed = suffix;
+		else if (seed.EndsWith(oppositeSuffix))
+			seed = seed.Substring(0, seed.Length - oppositeSuffix.Length);
+		else
+			seed = seed + suffix;
+	}
+}
