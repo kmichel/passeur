c611e77789b62b21881567418420f5f79e9f9515
Kevin Michel <kha.hem@gmail.com>
2014-08-23 07:00:21 +0200

    Add game object grid layout

---
+++ Assets/GridFiller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridFiller : MonoBehaviour {
+
+	public GameObject prototype;
+	public GridLayout layout;
+
+	void Start () {
+		if (layout != null && prototype != null) {
+			for (var column = 0; column < layout.size; ++column) {
+				for (var row = 0; row < layout.size; ++row) {
+					var instance = Object.Instantiate(prototype) as GameObject;
+					instance.SetActive(true);
+					instance.transform.localRotation = Quaternion.identity;
+					layout.AddItem(row, column, instance);
+               }
+			}
+		}
+	}
+}
---
+++ Assets/GridLayout.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GridLayout : MonoBehaviour {
+
+	private class GridItem {
+		public int row;
+		public int column;
+		public GameObject gameObject;
+	}
+
+	[Range(1, 100)]
+	public int size;
+
+	[System.NonSerialized]
+	private List<GridItem> items;
+
+	GridLayout() {
+		size = 1;
+		items = new List<GridItem>();
+	}
+
+	void Update () {
+		var scaling = 1.0f / size;
+		var spacing = scaling;
+		var offset = (spacing - 1.0f) * 0.5f;
+		for (var index = 0; index < items.Count; ++index) {
+			var item = items[index];
+			item.gameObject.transform.localScale =
+				new Vector3(scaling, scaling, scaling);
+			item.gameObject.transform.localPosition =
+				new Vector3(offset + spacing * item.row, offset + spacing * item.column, 0);
+		}
+	}
+
+	public void AddItem(int row, int column, GameObject gameObject) {
+		var item = new GridItem();
+		item.row = row;
+		item.column = column;
+		item.gameObject = gameObject;
+		gameObject.transform.parent = transform;
+		items.Add(item);
+	}
+}
